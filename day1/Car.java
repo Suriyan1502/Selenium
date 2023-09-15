74d38c5e4016a300f0a9d9ec75735235fbd8e7af
Suriyan1502 <111850763+Suriyan1502@users.noreply.github.com>
2023-09-15 09:48:17 +0530

    Add files via upload

---
+++ day1/Car.java
@@ -0,0 +1,25 @@
+package week1.day1;
+
+public class Car {
+	public void applyGear() {
+		// TODO Auto-generated method stub
+		System.out.println("Apply Gear");
+
+	}
+	public void switchOnAc() {
+		// TODO Auto-generated method stub
+		System.out.println("Switch On AC");
+	}
+	public void applyAcclerate() {
+		// TODO Auto-generated method stub
+		System.out.println("Apply Acclerate");
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Car bmw= new Car();
+		bmw.applyGear();
+		bmw.switchOnAc();
+		bmw.applyAcclerate();
+	}
+	
+}
---
+++ day1/Mobile.java
@@ -0,0 +1,25 @@
+package week1.day1;
+
+public class Mobile {
+	public void sendMessage() {
+		// TODO Auto-generated method stub
+		System.out.println("Mobile is send a Message");
+	}
+	public void shareDocuments() {
+		// TODO Auto-generated method stub
+		System.out.println("Share a Document");
+	}
+	public void makeCall() {
+		// TODO Auto-generated method stub
+		System.out.println("I make a Call");
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Mobile apple =new Mobile();
+		apple.sendMessage();
+		apple.shareDocuments();
+		apple.makeCall();
+	}
+
+}
